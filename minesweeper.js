2e34a6e30f4f598df0cf04f2b73c305d1c69d499
Alice Gatland <unrelated@gmail.com>
2019-02-28 09:19:42 +1300

    Code tidy and refactor

--- minesweeper.js
+++ minesweeper.js
@@ -1,47 +1,43 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+//initialise board size
+var difficulty = 'medium';
 
-
+//buttons to change board size
 var difficultyButtons = document.querySelector('.difficulty');
 difficultyButtons.addEventListener('click', function (event) {
   var buttonClicked = event.target;
-  checkDifficulty(buttonClicked.className);
+  difficulty = buttonClicked.className;
   startGame();
 });
 
+//button to reset board
 var resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', startGame);
 
 function checkDifficulty(difficulty) {
   switch (difficulty) {
     case 'easy':
-      console.log('easy');
+      //console.log('easy');
       rows = 4;
-
       break;
     case 'medium':
-      console.log('medium');
+      //console.log('medium');
       rows = 5;
-
       break;
     case 'hard':
-      console.log('hard');
+      //console.log('hard');
       rows = 6;
-
       break;
-
   }
 }
 
-var difficulty = 'medium';
-checkDifficulty(difficulty);
-
 function setupBoard() {
-  //empty cells array
+  //create empty cells array
   var board = {
     cells: []
   };
-  //create cellls
+  //create cellls - note that the board is always square so rows = cols
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < rows; j++) {
       board.cells.push({ row: i + 1, col: j + 1 });
@@ -50,65 +46,49 @@ function setupBoard() {
   board.cells.forEach(function (cell) {
     cell.hidden = true;
     cell.isMarked = false;
-    if (Math.random() >= 0.9) {
+    if (Math.random() >= 0.8) {
       cell.isMine = true;
     } else {
       cell.isMine = false;
     }
   });
-  console.log(board.cells);
+  //console.log(board.cells);
   return board;
 }
 
-
 function startGame() {
+  checkDifficulty(difficulty);
   board = setupBoard();
 
   document.addEventListener('click', checkForWin);
   document.addEventListener('contextmenu', checkForWin);
 
   for (var i = 0; i < board.cells.length; i++) {
-    surroundingMines =
-      board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
-  console.log(board.cells);
+  //console.log(board.cells);
 
+  //clear out the old board before re-creating
   document.querySelector('.board').innerHTML = '';
-  // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
 
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
 function checkForWin() {
-
   for (var i = 0; i < board.cells.length; i++) {
     var cell = board.cells[i];
+    //check that all mines are marked
     if (cell.isMine && !cell.isMarked) {
       return;
+    //check that all non-mines are no longer hidden
     } else if (!cell.isMine && cell.hidden) {
       return;
     }
   }
   var audioWin = document.querySelector(".audioWin");
   audioWin.play();
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
   lib.displayMessage('You win!')
-
 }
 
-
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines(cell) {
   var surroundingCells = getSurroundingCells(cell.row, cell.col);
   var mineCount = 0;
@@ -119,6 +99,3 @@ function countSurroundingMines(cell) {
   });
   return mineCount;
 }
-
-
-
